a62dcfaafda633ca7e00b1dd929ecc3fbd2de6b5
CJ Uberroth <cuberroth@gmail.com>
2022-03-30 21:39:43 -0500

    Created standin HomeScreen

---
+++ BeyondSW5E/src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>We have a project!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default HomeScreen
