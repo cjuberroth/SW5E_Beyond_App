41ec16209fa28522c836ad7cd61c2b6f16b24271
CJ Uberroth <cuberroth@gmail.com>
2022-05-18 10:20:21 -0500

    Created the powercard and powercardlist components. This is a bit redundant and there is likely a better way to use wider reaching components for this. Created the spellsscreen for the powers base screen. Implemented the pop up dialogs for the powers in the list.

--- BeyondSW5E/src/screens/SpellsScreen.js
+++ BeyondSW5E/src/screens/SpellsScreen.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Text, View, StyleSheet, Button } from 'react-native'
+import swapi from '../api/swapi'
+import PowerCardList from '../components/PowerCardList'
 
 const SpellsScreen = () => {
+    const [power, setPower] = useState([])
+
+    const searchApi = async () => {
+        const response = await swapi.get('/power');
+        setPower(response.data)
+    }
+
+    useEffect(() => { searchApi() }, [])
+
     return (
-        <View>
-            <Text>Force/Tech Powers Screen</Text>
+        <View style = { styles.screenContainer }>
+            <Text style = { styles.headerStyle }>Powers Screen</Text>
+            <Text>Displaying {power.length} powers.</Text>
+            {
+                power.length === 0
+                ? <Text>Equipment is Empty</Text>
+                : <PowerCardList
+                      powers = { power }
+                  />
+            }
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    screenContainer: {
+        alignItems: 'center',
+        flex: 1
+    },
+    headerStyle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    }
+})
 
 export default SpellsScreen
