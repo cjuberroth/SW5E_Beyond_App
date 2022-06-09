c98d8cafc1e8abf8dc80a878097e01b23a30c9a2
CJ Uberroth <cuberroth@gmail.com>
2022-06-08 21:20:03 -0500

    Moved the spells screen to the manage spells screen and reversed the spells screen.

--- BeyondSW5E/src/screens/SpellsScreen.js
+++ BeyondSW5E/src/screens/SpellsScreen.js
@@ -1,43 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { Text, View, StyleSheet, Button } from 'react-native'
-import swapi from '../api/swapi'
-import PowerCardList from '../components/PowerCardList'
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
 
 const SpellsScreen = () => {
-    const [power, setPower] = useState([])
-
-    const searchApi = async () => {
-        const response = await swapi.get('/power');
-        setPower(response.data)
-    }
-
-    useEffect(() => { searchApi() }, [])
-
     return (
-        <View style = { styles.screenContainer }>
-            <Text style = { styles.headerStyle }>Powers Screen</Text>
-            <Text>Displaying {power.length} powers.</Text>
-            {
-                power.length === 0
-                ? <Text>Equipment is Empty</Text>
-                : <PowerCardList
-                      powers = { power }
-                  />
-            }
+        <View>
+            <Text>Force/Tech Powers Screen</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    screenContainer: {
-        alignItems: 'center',
-        flex: 1
-    },
-    headerStyle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        alignSelf: 'center'
-    }
-})
+const styles = StyleSheet.create({})
 
 export default SpellsScreen
