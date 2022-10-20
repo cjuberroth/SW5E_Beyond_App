738a12a33fab3ca35c5656eefdd50d78845d735c
CJ Uberroth <cuberroth@gmail.com>
2022-10-19 20:07:44 -0500

    Implemented a rudimentary notes screen There will likely need to be code reapplied which outfits the keyboard for android and ios.

--- BeyondSW5E/src/screens/NotesScreen.js
+++ BeyondSW5E/src/screens/NotesScreen.js
@@ -1,14 +1,60 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useContext } from 'react'
+import { Text, View, StyleSheet, ImageBackground, TextInput } from 'react-native'
+import CharacterContext from '../context/CharacterContext'
+import Header from '../components/Header'
+import HeaderCollapsed from '../components/HeaderCollapsed'
+import HeaderContext from '../context/HeaderContext'
+import AppStyles from '../styles/AppStyles'
 
 const NotesScreen = () => {
+    const charInfo = useContext(CharacterContext).characterInformation
+    const background = charInfo.background
+    const characteristics = charInfo.characteristics
+    const flexValue = useContext(HeaderContext).headerUtils.flexValue
+    const headerCollapsed = useContext(HeaderContext).headerUtils.isCollapsed
     return (
-        <View>
-            <Text>Notes</Text>
+        <View style = { AppStyles.globalStyles.parentContainerView }>
+            <View style={ styles.header }>
+                {!headerCollapsed ? <Header /> : <HeaderCollapsed />}
+            </View>
+            <View style={{flex: flexValue}}>
+                <ImageBackground style={ AppStyles.globalStyles.screenBackground }
+                    source={ require('../../assets/header-background.jpg') }>
+                        <View style={styles.contentView}>
+                            <Text style={styles.screenHeader}>Notes</Text>
+                            <TextInput
+                                style={styles.input}
+                                multiline={true}
+                                returnKeyType='none'
+                            />
+                        </View>
+                </ImageBackground>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    header: {
+        flex: 1
+    },
+    input: {
+        borderWidth: 3,
+        borderColor: '#4A0C05',
+        height: '100%',
+        flex: 1,
+        color: 'white',
+        padding: 4,
+        textAlignVertical: 'top'
+    },
+    contentView: {
+        padding: 10,
+        flex: 1
+    },
+    screenHeader: {
+        fontSize: 18,
+        color: 'white'
+    }
+})
 
 export default NotesScreen
