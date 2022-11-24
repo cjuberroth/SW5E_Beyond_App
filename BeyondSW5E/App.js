8c71b648c069dbf41c8d0d2ff4e1c5aca6abc2fb
Anthony Yannotta <anthonyyannotta@hotmail.com>
2022-11-23 21:31:32 -0600

    Added icon and splash assets; preloading images

--- BeyondSW5E/App.js
+++ BeyondSW5E/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import MainNavigator from './src/navigators/MainNavigator'
+import { Asset } from 'expo-asset'
+import { AppLoading } from 'expo'
 import { CharacterProvider } from './src/context/CharacterContext'
 import { HeaderProvider } from './src/context/HeaderContext'
 
 function App() {
+    const [isReady, setReady] = useState(false)
+
+    const _cacheResourcesAsync = async () => {
+        const images = [require('./assets/header-background.jpg'), require('./assets/header-background-upsidedown.jpg')]
+
+        const cacheImages = images.map(image => {
+            return Asset.fromModule(image).downloadAsync()
+        })
+        return Promise.all(cacheImages)
+    }
+    
     return (
         <CharacterProvider>
             <HeaderProvider>
